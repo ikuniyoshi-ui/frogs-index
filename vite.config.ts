9c22f7fca1c7fba2bc3da5ecf3aced8df01f8a30
ikuniyoshi-ui <i.kuniyoshi@frogs-corp.jp>
2026-01-28 12:53:05 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/frogs-index/', // ★リポジトリ名が frogs-index なので、ここも合わせる
+  base: '/frogs-index/', // リポジトリ名に合わせています
 })
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
